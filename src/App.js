72e377d4337baccc5c8cf9cb183a9c6ef2b1603f
henry lay <henry88lay@gmail.com>
2020-07-13 21:56:32 +1000

    playing around and getting the basic structure in place for my personal webpage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from "./components/header";
+import Container from "./components/container"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <Container>
+        
+      </Container>
     </div>
   );
 }
