4c2f3c714574e5d9b5a67a795e92d6b71babe8f8
henry lay <henry88lay@gmail.com>
2020-07-13 22:27:47 +1000

    made changes towards the navbar of the site

--- src/components/header.js
+++ src/components/header.js
@@ -11,12 +11,16 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
+  customizeToolbar: {
+    minHeight: 24
+  },
   menuButton: {
     marginRight: theme.spacing(2),
   },
   title: {
     flexGrow: 1,
   },
+
 }));
 
 export default function ButtonAppBar() {
@@ -25,14 +29,11 @@ export default function ButtonAppBar() {
   return (
     <div className={classes.root}>
       <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
+        <Toolbar className={classes.customizeToolbar}>
           <Typography variant="h6" className={classes.title}>
-            Henry Lay
+            <Button color="inherit">Home</Button>
+            <Button color="inherit">My Gear</Button>
           </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </div>
